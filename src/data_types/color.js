6e6d2f76ea7cbe68b3d5aa85522bd20132b2d251
PatrickKR <mailpatrickkr@gmail.com>
2021-05-22 21:49:11 +0900

    feat: color.js add alpha support

--- src/data_types/color.js
+++ src/data_types/color.js
@@ -9,22 +9,38 @@ class Color {
   constructor(val) {
     switch (typeof val) {
       case "string":
-        if (val.length != 0) {
-          val = val.replace("#", "");
-          var c = val.repeat(6).substr(0, 6);
-          this.R = parseInt(c.substr(0, 2), 16);
-          this.G = parseInt(c.substr(2, 2), 16);
-          this.B = parseInt(c.substr(4, 2), 16);
+        val = val.replace("#", "");
+
+        if ([3, 4, 6, 8].includes(val.length)) {
+          if (val.length === 3 || val.length === 4) {
+            val = val.split('').map((v) => v + v).join('')
+          }
+          this.R = parseInt(val.substr(0, 2), 16) || 255;
+          this.G = parseInt(val.substr(2, 2), 16) || 255;
+          this.B = parseInt(val.substr(4, 2), 16) || 255;
+          this.A = parseInt(val.substr(6, 2), 16) || 255;
+        } else if ([1, 2].includes(val.length)) {
+          let c = parseInt(val, 16) || 255;
+          this.R = c;
+          this.G = c;
+          this.B = c;
+          this.A = 255;
         }
         break;
-
       case "number":
         val = Math.abs(val);
-        var c = "000000" + val.toString(16);
-        c = c.substr(val.length - 6, 6);
-        this.R = parseInt(c.substr(0, 2), 16);
-        this.G = parseInt(c.substr(2, 2), 16);
-        this.B = parseInt(c.substr(4, 2), 16);
+        let c;
+        if (val >= 2 ** 24 && val < 2 ** 32) { // includes alpha value
+          c = val.toString(16).padStart(8, '0');
+        } else if (val >= 0 && val < 2 ** 24) {
+          c = val.toString(16).padStart(6, '0');
+        } else {
+          break;
+        }
+        this.R = parseInt(c.substr(0, 2), 16) || 255;
+        this.G = parseInt(c.substr(2, 2), 16) || 255;
+        this.B = parseInt(c.substr(4, 2), 16) || 255;
+        this.A = parseInt(c.substr(6, 2), 16) || 255;
         break;
     }
   }
@@ -51,31 +67,35 @@ class Color {
   B = 255;
 
   /**
+   * A is an integer between 0 ~ 255 deciding opacity of the color.
+   *
+   * You can use 'color.A = 0;' to set its opacity.
+   */
+
+  A = 255;
+
+  /**
    * Returns a value converted to HEX string. DOES NOT INCLUDE #.
    */
   toString() {
-    let arr = Object.keys(this).filter((x) => x.length == 1);
-    for (var i = 0; i < 3; i++) {
+    let arr = Object.keys(this).filter((x) => x.length === 1);
+    for (let i = 0; i < 4; i++) {
       if (this[arr[i]] < 0) this[arr[i]] = 0;
       if (this[arr[i]] > 255) this[arr[i]] = 255;
     }
-    var _r = Math.floor(this.R).toString(16);
-    var _g = Math.floor(this.G).toString(16);
-    var _b = Math.floor(this.B).toString(16);
-    if (_r.length != 2)
-      _r = _r.length > 2 ? _r.substr(0, 2) : "0".repeat(2 - _r.length) + _r;
-    if (_g.length != 2)
-      _g = _g.length > 2 ? _g.substr(0, 2) : "0".repeat(2 - _g.length) + _g;
-    if (_b.length != 2)
-      _b = _b.length > 2 ? _b.substr(0, 2) : "0".repeat(2 - _b.length) + _b;
-    return `${_r}${_g}${_b}`;
+    let _r = Math.floor(this.R).toString(16).padStart(2, '0');
+    let _g = Math.floor(this.G).toString(16).padStart(2, '0');
+    let _b = Math.floor(this.B).toString(16).padStart(2, '0');
+    let _a = Math.floor(this.A).toString(16).padStart(2, '0');
+    return `${_r}${_g}${_b}${_a}`;
   }
 
   /**
    * Returns a value converted to a number.
+   * @returns {number} a converted value
    */
   toNumber() {
-    return Number(this.toString());
+    return parseInt(this.toString(), 16);
   }
 }
 
