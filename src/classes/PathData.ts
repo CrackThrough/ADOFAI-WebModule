231b2d106794084c6e5934685e56ad00fb673e9c
CrackThrough <59867573+CrackThrough@users.noreply.github.com>
2021-08-05 05:04:45 +0900

    Finish angle calculation utils

--- src/classes/PathData.ts
+++ src/classes/PathData.ts
@@ -1,268 +1,3 @@
-// /**
-//  * Class for easy manipulation of paths.
-//  *
-//  * Used directly in `pathData: "..."`.
-//  *
-//  * Contains every types of path. Do `Object.keys(AdofaiMapPathData.PATH_LIST)` to get all list of Path in String.
-//  */
-
-// class AdofaiMapPathData {
-//     /**
-//      * List of the path type as String.
-//      */
-//     static PATH_LIST = [
-//         "U",
-//         "R",
-//         "L",
-//         "D",
-//         "E",
-//         "C",
-//         "Q",
-//         "Z",
-//         "H",
-//         "G",
-//         "T",
-//         "J",
-//         "M",
-//         "B",
-//         "F",
-//         "N",
-//         "!",
-//         "5",
-//         "6",
-//         "7",
-//         "8",
-//         "q",
-//         "W",
-//         "x",
-//         "V",
-//         "Y",
-//         "A",
-//         "p",
-//         "o",
-//     ];
-
-//     /**
-//      * List of the path's absolute angle as Number. (Use same index with PATH_LIST to get the path key)
-//      */
-//     static ABSOLUTE_ANGLE_LIST = [
-//         90,
-//         180,
-//         360,
-//         270,
-//         135,
-//         225,
-//         45,
-//         315,
-//         30,
-//         60,
-//         120,
-//         150,
-//         210,
-//         240,
-//         300,
-//         330,
-//         0,
-//         108,
-//         252,
-//         900 / 7,
-//         1620 / 7,
-//         75,
-//         15,
-//         345,
-//         285,
-//         255,
-//         195,
-//         165,
-//         105,
-//     ];
-
-//     /**
-//      * Code of the path.
-//      */
-//     code = String();
-
-//     /**
-//      * Absolute angle of the path.
-//      */
-//     absoluteAngle = NaN;
-
-//     /**
-//      * Create a path using a constructor.
-//      * @param {String | Number} val Base value to create a instance of.
-//      *
-//      * When you input String: You are creating a path using a **Tile Code**.
-//      *
-//      * When you input Number: You are creating a path using a **Absolute Angle of the tile**. If you want to get a twirled one's angle, input `360 - angle`.
-//      *
-//      * Inputting number might return instance with empty values when no corresponding tile is found.
-//      *
-//      * @param {{findNearestTileByAngle: boolean, useSmallerAngleOnOverlapping: boolean, fallback: String | null}} settings Customizable settings to generate a tile.
-//      */
-//     constructor(
-//         val,
-//         settings = {
-//             /**
-//              * Whether to try to find the most closest angle when the given angle does not exist
-//              */
-//             findNearestTileByAngle: false,
-
-//             /**
-//              * Whether to use the tile with a higher (<-- false) or smaller (<-- true) angle when difference of nearest angle is both the same.
-//              */
-//             useSmallerAngleOnOverlapping: true,
-
-//             /**
-//              * Use this to return a specific tile as a "fallback tile" instead of empty class.
-//              *
-//              * You should input a tile code here.
-//              */
-//             fallback: null,
-//         }
-//     ) {
-//         if (!settings || Object.keys(settings || {}).length !== 3)
-//             settings = {
-//                 findNearestTileByAngle: false,
-//                 useSmallerAngleOnOverlapping: true,
-//                 fallback: null,
-//             };
-
-//         if (
-//             settings.fallback &&
-//             !AdofaiMapPathData.PATH_LIST.includes(settings.fallback)
-//         ) {
-//             settings.fallback = null;
-//         }
-
-//         switch (typeof val) {
-//             case "string":
-//                 const tileCode = val[0];
-
-//                 if (AdofaiMapPathData.PATH_LIST.includes(tileCode)) {
-//                     this.code = tileCode;
-//                     this.absoluteAngle =
-//                         AdofaiMapPathData.ABSOLUTE_ANGLE_LIST[
-//                             AdofaiMapPathData.PATH_LIST.indexOf(tileCode)
-//                         ];
-//                     if (this.code == null || this.absoluteAngle == null)
-//                         return new AdofaiMapPathData(settings.fallback);
-//                 } else if (settings.fallback != null) {
-//                     return new AdofaiMapPathData(settings.fallback);
-//                 }
-
-//                 break;
-//             case "number":
-//                 if (AdofaiMapPathData.ABSOLUTE_ANGLE_LIST.includes(val)) {
-//                     this.absoluteAngle = val;
-//                     this.code =
-//                         AdofaiMapPathData.PATH_LIST[
-//                             AdofaiMapPathData.ABSOLUTE_ANGLE_LIST.indexOf(val)
-//                         ];
-//                     if (this.code == null || this.absoluteAngle == null)
-//                         return new AdofaiMapPathData(settings.fallback);
-//                 } else {
-//                     if (settings.findNearestTileByAngle) {
-//                         const sorted =
-//                             AdofaiMapPathData.ABSOLUTE_ANGLE_LIST.sort();
-//                         let nearest = 0;
-//                         let dist = Infinity;
-//                         sorted.forEach((n) => {
-//                             const d = Math.abs(nearest - val);
-//                             if (d < dist) {
-//                                 dist = d;
-//                                 nearest = n;
-//                             } else if (d === dist && n !== nearest)
-//                                 nearest = settings.useSmallerAngleOnOverlapping
-//                                     ? n > nearest
-//                                         ? nearest
-//                                         : n
-//                                     : n < nearest
-//                                     ? nearest
-//                                     : n;
-//                         });
-//                         const index =
-//                             AdofaiMapPathData.ABSOLUTE_ANGLE_LIST.indexOf(
-//                                 nearest
-//                             );
-//                         if (index != null) {
-//                             this.code = AdofaiMapPathData.PATH_LIST[index];
-//                             this.absoluteAngle = nearest;
-//                             if (this.code == null || this.absoluteAngle == null)
-//                                 return new AdofaiMapPathData(settings.fallback);
-//                         } else return new AdofaiMapPathData(settings.fallback);
-//                     } else {
-//                         if (settings.fallback != null) {
-//                             return new AdofaiMapPathData(settings.fallback);
-//                         }
-//                     }
-//                 }
-//                 break;
-//         }
-//     }
-
-//     /**
-//      * Calculate the Angle between tiles. THIS IS NOT FOR ! TILES.
-//      * ! Tile can be calculated differently rather than getting relative angle.
-//      * For example, String `RD!R`'s R.A is 180, 270, (0, ) 270.
-//      * You can do `360 - TileD.absoluteAngle` and use its angle to create and use a temporary tile to calculate angle between `D!` and `R`.
-//      *
-//      * Correct Usage:
-//      * GetRelativeAngle(tileA, tileB, false);
-//      * GetRelativeAngle(tileA.code + "5768888755786", tileB, true);
-//      *
-//      * @param {AdofaiMapPathData | String} thisTile First tile to be a base for second tile's angle. (Input String when calculating all of 5, 6, 7, 8. They should be calculated at once)
-//      * @param {AdofaiMapPathData} nextTile Second tile to base on the first tile
-//      * @param {boolean} twirled Whether the planet is twirled or not
-//      */
-//     static GetRelativeAngle(thisTile, nextTile, twirled) {
-//         let absTiles = ["5", "6", "7", "8"];
-//         let t, n;
-//         if (absTiles.includes(nextTile.code)) {
-//             n = nextTile.absoluteAngle;
-//         } else {
-//             // normal calculation
-//             if (typeof thisTile == "string") {
-//                 // calc as 5 6 7 8 str
-//                 const lastChar = thisTile[thisTile.length - 1];
-//                 if (absTiles.includes(lastChar)) {
-//                     // it ends with 5 6 7 8
-//                     const numbers = [];
-//                     for (let i = 0; i < 4; i++) {
-//                         const regex = new RegExp(`[^${i + 5}]`, "g");
-//                         numbers.push(thisTile.replace(regex, "").length);
-//                     }
-//                     t =
-//                         (72 * numbers[0] + // 180 - 108
-//                             288 * numbers[1] + // 252 
-//                             (900 / 7) * numbers[2] +
-//                             (1620 / 7) * numbers[3]) %
-//                         360;
-//                 } else {
-//                     // it does not end with 5 6 7 8
-//                     const simulatedTile = new this(lastChar);
-//                     if (isNaN(simulatedTile.absoluteAngle)) {
-//                         // fallback, return NaN
-//                         return NaN;
-//                     } else t = simulatedTile.absoluteAngle;
-//                 }
-//             } else t = thisTile.absoluteAngle;
-//         }
-//         let result = (n - t + 540) % 360;
-//         if (twirled) result = 360 - result;
-//         if (result === 0) result = 360;
-//         return result;
-//     }
-
-//     /**
-//      * Calculate the milliseconds between 2 tiles.
-//      * @param {Number} Angle Angle between 2 tiles
-//      * @param {Number} BPM BPM on that specific tile
-//      */
-//     static GetMsBetweenTile(Angle, BPM) {
-//         return (1000 * Angle) / (3 * BPM);
-//     }
-// }
-
 import { PathCode } from "../typings";
 
 /**
@@ -332,7 +67,7 @@ export default class PathData {
         'N': 330,
         'x': 345,
         'L': 360,
-        // * ABSOLUTE ANGLE TILES
+        // * ALWAYS RELATIVE ANGLE TILES
         '5': 108,
         '6': 252,
         '7': 900 / 7,
@@ -421,8 +156,15 @@ export default class PathData {
      * @returns relative angle between two tiles
      */
     static GetRelativeAngleByCode(prevTile: string, thisTile: string, twirled = false): number {
-        let prevAngle = this.GetRelativeAngleOffset(prevTile);
+        let prevAngle = this.GetRelativeAngleOffset(prevTile),
+            thisAngle = this.FindTileFromDict(thisTile as PathCode)[1] ?? NaN;
+        
+        // Calculate angle between two tiles
+        let result = (thisAngle - prevAngle + 540) % 360;
+        if (twirled) result = 360 - result;
+        if (result == 0) result = 360;
 
+        return result;
     }
 
     /**
@@ -453,18 +195,53 @@ export default class PathData {
         // True offset from R to add to calculated value
         startingAngle = 180 - startingAngle;
 
-        // If there's any midspin tile inside
-        if (tileCodes.includes('!')) {
-            // For easier calculation of midspin tiles we just split it to '!' first
-            let tileCodesSplit = tileCodes.split('!');
-            tileCodesSplit.forEach(t => {
-                // TODO: Write a code that 
+        // Remove the starting tile if starting tile isn't always relative
+        if (!this.ALWAYS_RELATIVE_TILE_CODES.includes(tileCodes[0])) tileCodes = tileCodes.substr(1);
+
+        // Pre-substr needed string
+        const ALWAYS_RELATIVE_TILE_CODES =
+            this.ALWAYS_RELATIVE_TILE_CODES
+                .substr(0, this.ALWAYS_RELATIVE_TILE_CODES.length - 1)
+                .split('');
+
+        let totalTileAngle = 0,
+            result = NaN;
+
+        // For easier calculation of midspin tiles we just split it to '!' first
+        let tileCodesSplit = tileCodes.split('!');
+        tileCodesSplit.forEach((t, i) => {
+            let curTileAngle = 0,
+                isLast = i == tileCodesSplit.length - 1;
+
+            // Get amount of specific tile codes inside the string
+            ALWAYS_RELATIVE_TILE_CODES.forEach((c, k) => {
+                let charAmounts = t.length - t.replace(new RegExp(c, 'g'), '').length,
+                    offsetAngle = this.ALWAYS_RELATIVE_TILE_OFFSETS[k];
+                
+                if (twirled) offsetAngle = 360 - offsetAngle;
+
+                // Multiply by amounts
+                curTileAngle += charAmounts * offsetAngle;
             });
-        }
+
+            if (!isLast) {
+                // If midspin tile(s) afterwards is/are there
+                totalTileAngle += this.GetRelativeMidspinAngle(
+                    curTileAngle,
+                    this.FindTileFromDict(tileCodesSplit[i + 1][0] as PathCode)[1] ?? NaN,
+                    twirled);
+            } else {
+                // If there isn't any midspin tile, sum everything in the list and return
+                result = (totalTileAngle + curTileAngle) % 360;
+                if (result == 0) result = 360;
+            }
+        });
+
+        return result;
     }
 
     private static GetRelativeMidspinAngle(prevAngle: number, nextAngle: number, twirled = false): number {
-        // Middle angle to append
+        // Middle angle to append to result
         let midAngle: number,
             diff: number = Math.abs(prevAngle - nextAngle);
 
